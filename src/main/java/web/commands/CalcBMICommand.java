c0a4275fd079edbbdf9df2daaebcadcae2761937
Rasmus Olsson <cph-ro53@cphbusiness.dk>
2021-04-23 10:16:48 +0200

    added database functuality

--- src/main/java/web/commands/CalcBMICommand.java
+++ src/main/java/web/commands/CalcBMICommand.java
@@ -1,7 +1,10 @@
 package web.commands;
 
 import business.exceptions.UserException;
+import business.services.BmiFacade;
 import business.services.BmiUtil;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -9,27 +12,38 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CalcBMICommand extends  CommandUnprotectedPage {
 
-
+    private BmiFacade bmiFacade;
 
 
     public CalcBMICommand(String pageToShow) {
+
         super(pageToShow);
+        this.bmiFacade = new BmiFacade(database);
     }
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
 
+        int user_id =1;
         double height = 0.0;
         double weight = 0.0;
         double bmi = 0.0;
         String category ="";
         String gender = request.getParameter("gender");
         int sport_id = Integer.parseInt(request.getParameter("sport"));
+
         String[] hobbies =request.getParameterValues("hobby");
-        List<String> hobbyList = null;
+        List<String> hobbyListStrings = null;
+
         if(hobbies != null){
-            hobbyList = Arrays.asList(hobbies);
+            hobbyListStrings = Arrays.asList(hobbies);
         }
+
+        List<Integer> hobbyListIntegers = new ArrayList<>();
+        for(String hobbyListItem:hobbyListStrings){
+            hobbyListIntegers.add(Integer.parseInt(hobbyListItem));
+        }
+
 
         try {
             height = Double.parseDouble(request.getParameter("height"));
@@ -49,8 +63,17 @@ public class CalcBMICommand extends  CommandUnprotectedPage {
         request.setAttribute("category",category);
         request.setAttribute("gender",gender);
         request.setAttribute("sport_id",sport_id);
-        request.setAttribute("hobbies", hobbyList);
+        request.setAttribute("hobbies", hobbyListIntegers);
 
+        bmiFacade.insertBmiEntry(
+                bmi,
+                height,
+                weight,
+                category,
+                gender,
+                sport_id,
+                user_id,
+                hobbyListIntegers);
 
 
         return pageToShow;
